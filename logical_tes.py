c4dea32be3ff2d9069f3735f0b5cf10ec215b062
FerdyBerlianoP <ferdyberliano5@gmail.com>
2022-10-31 13:36:30 +0700

    Update Logical test united tractor

--- logical_tes.py
+++ logical_tes.py
@@ -1,11 +1,14 @@
 def kandag():
     bebek = int(input("jumlah bebek :"))
-    batas = 12
+    batas = int(input("batas per kandang : "))
     rak = 1
-    while (bebek>batas):
-        bebek=bebek-batas
-        batas = batas-2
-        rak = rak+1
-    print("Jumlah susunan : ", rak)
+    if(bebek<=42):
+        while (bebek>batas):
+            bebek=bebek-batas
+            batas = batas-2
+            rak = rak+1
+        print("Jumlah susunan : ", rak)
+    else:
+        print("Tidak bisa memasukkan bebek lebih dari 42 ekor!")
 
 kandag()
